1c99dc90ba804e63e1af54f7d563e81274afd6bd
Rivo Bifa <69102360+anotherbeef@users.noreply.github.com>
2022-02-24 14:13:43 +0700

    Update error
    Fixed

--- src/data/DataCalcLifeIns.js
+++ src/data/DataCalcLifeIns.js
@@ -299,8 +299,7 @@ export const DataInputCalc = {
 //       type: 'link-page',
 //       path: DataLinkPage.edu_netoVsBruto,
       type: 'link',
-//       path: 'https://www.ojk.go.id/id/kanal/iknb/regulasi/asuransi/peraturan-ojk/Documents/Pages/POJK-Nomor-23-Produk-Asuransi-Pemasaran-Produk-Asuransi/SALINAN-POJK%20Produk%20Asuransi%20.pdf',
-      path: 'https://google.com',
+      path: 'https://www.ojk.go.id/id/kanal/iknb/regulasi/asuransi/peraturan-ojk/Documents/Pages/POJK-Nomor-23-Produk-Asuransi-Pemasaran-Produk-Asuransi/SALINAN-POJK%20Produk%20Asuransi%20.pdf',
     },
     {
       text: '.',
